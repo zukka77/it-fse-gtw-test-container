694e24970d7805648acf40b3239917b822cf3d3b
Andrea Zucchelli <zukka77@gmail.com>
2023-07-11 15:47:47 +0200

    feat:replace oracle with pg * This commit replace oracle with postgres. * Optimizes a little the mongo collection import process * Configures the postgres database * Injects the postgres dependency * eliminates the "brided" network mode * eliminatese the need for "extra_hosts" * uses image urls compatible with podman

--- it-fse-terminology/mongo/mongo-init.sh
+++ it-fse-terminology/mongo/mongo-init.sh
@@ -2,28 +2,20 @@
 
 echo "Starting"
 
-zstd_folder=$(pwd)/docker-entrypoint-initdb.d/mongo-dump/input
-collections_folder=$(pwd)/docker-entrypoint-initdb.d/mongo-dump/output
+zstd_folder=/docker-entrypoint-initdb.d/mongo-dump/input
+#No need to write in host fs
+collections_folder=/tmp/output
 
 echo "Input folder: $zstd_folder"
-
-if [ ! -d "$collections_folder" ]; then
-    echo "Output folder: - $collections_folder - does not exist...it will be created"
-    mkdir $collections_folder || { echo "Failed to create folder $collections_folder"; exit 1; }
-else
-    echo "Collections folder already created..."
-fi
+mkdir -p $collections_folder || { echo "Failed to create folder $collections_folder"; exit 1; }
 
 echo "Decompressing files (.zstd)"
-
 # Enter the target folder containing .zstd
 cd $zstd_folder || { echo "Failed to enter folder ${zstd_folder}"; exit 1; }
 
-files=$(ls)
 
 # Iterate and extract
-for file in $files
-do
+for file in *zst; do
   filename="${file%%.*}"
   echo "Decompressing: $filename"
   zstd -d < $file > $collections_folder/$filename.json || { echo "Failed to decompress $file"; exit 1; }
@@ -31,14 +23,11 @@ done
 
 # Save on mongo
 echo "Importing collections on MongoDB"
-
-mongoimport --db "fse" --collection "schematron" --file "$collections_folder/schematron.json" --jsonArray
-mongoimport --db "fse" --collection "terminology" --file "$collections_folder/terminology.json" --jsonArray
-mongoimport --db "fse" --collection "transform" --file "$collections_folder/transform.json" --jsonArray
-mongoimport --db "fse" --collection "dictionary" --file "$collections_folder/dictionary.json" --jsonArray
+for file in ${collections_folder}/*json;do
+collection_name="${file%%.*}"
+mongoimport --db "fse" --collection "${collection_name}" --file $file --jsonArray
+done
 
 echo "Removing working folder"
-
 rm -rf $collections_folder || { echo "Failed to remove folder $collections_folder"; exit 1; }
-
 echo "Done"
--- it-fse-terminology/term-runner/entrypoint.sh
+++ it-fse-terminology/term-runner/entrypoint.sh
@@ -7,8 +7,18 @@ fi
 
 mkdir -p /app
 cd /app
-git clone --depth=1 --branch $BRANCH $URL .
-git config pull.ff only
-git pull
 
-exec mvn spring-boot:run -Dspring-boot.run.profiles=docker -Dspring-boot.run.arguments="--management.server.port=$PORT --server.port=$PORT"
+
+if [ ! -d /app/.git ];then #clone only the first time
+    git clone --depth=1 --branch $BRANCH $URL . ||  { echo "fatal: failed to clone branch '$BRANCH' at url '$URL'" && exit 1; }
+    git config pull.ff only
+fi
+git pull ||  { echo "fatal: failed to pull branch '$BRANCH' at url '$URL'" && exit 1; }
+
+echo "running: mvn spring-boot:run -Dspring-boot.run.profiles=docker -Dspring-boot.run.arguments=\"--management.server.port=$PORT --server.port=$PORT\""
+#Put configuration in to place
+cp ../application-docker.properties .
+#Inject Postgres jdbc dependency in pom2.xml
+sed 's/<dependencies>/<dependencies>\n<!--postgres-->\n<dependency>\n<groupId>org.postgresql<\/groupId>\n<artifactId>postgresql<\/artifactId>\n<version>42.6.0<\/version>\n<\/dependency>\n<!--postgres-->/g' pom.xml > pom2.xml
+#We use pom2.xml with injected dependency
+exec mvn -f pom2.xml spring-boot:run -Dspring-boot.run.profiles=docker -Dspring-boot.run.arguments="--management.server.port=$PORT --server.port=$PORT"
